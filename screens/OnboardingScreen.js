107d24ee2785528b05bd25f3ba2ea1e2591c6504
william-monroy <william_monroy777@hotmail.com>
2021-06-19 09:20:05 -0500

    redesign app

---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+import Onboarding from 'react-native-onboarding-swiper';
+
+const OnboardingScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <Text>Onboarding Screen</Text>
+            <Button
+                title="Click here"
+                onPress={() => navigation.navigate("Login")}
+            />
+        </View>
+    )
+}
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+})
